0c687edf008616c6f3d4ec4c27e0401c2b3e6b38
Raphael Carubbi Neto <rcarubbi@gmail.com>
2026-07-12 09:38:46 +0100

    fix(three): update /players/[slug] camera

--- src/components/three/ThreeBackground.tsx
+++ src/components/three/ThreeBackground.tsx
@@ -825,8 +825,8 @@ const Scene = memo(function Scene() {
       targetPos.current.set(-2.864, 0.377, 0.805);
       setCamera({ lookX: 0, lookY: -1, lookZ: 0, fov: 95, useRotation: false, override: false });
     } else if (pathname.startsWith("/players/")) {
-      targetPos.current.set(-0.26, 0.086, 0.1);
-      setCamera({ lookX: 0, lookY: -1, lookZ: 0, fov: 45, useRotation: false, override: false });
+      targetPos.current.set(-0.317, 0.13, 0.092);
+      setCamera({ lookX: 0, lookY: -1, lookZ: 0, fov: 35, useRotation: false, override: false });
     } else if (pathname === "/players") {
       targetPos.current.set(1.881, 0.184, 0.96);
       setCamera({ lookX: 0, lookY: -1, lookZ: 0, fov: 60, useRotation: false, override: false });
